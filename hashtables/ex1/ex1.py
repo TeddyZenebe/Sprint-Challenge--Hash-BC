575eec3c7847144dc1f47bd91d6ddf4ad77f2c45
teddyzenebe <tfikrie@yahoo.com>
2020-04-19 23:40:25 -0400

    hash table 1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,36 +8,22 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-    for i in range(len(weights)):
-            hash_table_insert(ht, weights[i], i)
-    if len(weights) <= 1:
-        print_answer(None)
-    elif len(weights) == 2:
-        a= weights[0]
-        b= weights[1]
-        if a + b == limit:
-            if a > b:
-                answer = (0, 1) #index of a and b
-                return answer
+    #Inserts weight and its index into hash table
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+    #finds two weights whose sum of weights equals the weight limit
+    for j in range(length):
+        #the next weight which is required to satsfy the limit
+        next_weight = limit-weights[j]
+        #get the index of the weight using the function imported
+        next_weight_index = hash_table_retrieve(ht, next_weight)
+        #if the the next index exist, compare the index value and place in the proper place (large, small)
+        if next_weight_index:
+            if j > next_weight_index:
+                return (j, next_weight_index) 
             else:
-                answer = (1, 0) #index of a and b
-                return answer 
-        else:
-            print_answer(None)     
-    else:
-        for pair in ht.storage:
-            for k in weights:
-                a = k
-                b = pair.key
-                while a + b != limit:
-                    print(a)
-                    if a > b:
-                        c = hash_table_retrieve(ht, a)
-                        d = hash_table_retrieve(ht, b)
-                        answer = (c, d)
-                    else:
-                        answer = (d,c)
-                return answer
+                return(next_weight_index, j)  
+    return None
     
 
 def print_answer(answer):
